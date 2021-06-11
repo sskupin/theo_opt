ce8d745c7be2f9c72aa040a255edab3a62ab6d71
Stefan Skupin <stefan.skupin@web.de>
2021-06-11 10:08:03 +0200

    FPS.py

---
+++ FPS.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import tkinter as Tk
+import gui_stuff as gui
+
+mpl.rcParams['font.family'] = 'sans-serif'
+mpl.rc('text', usetex=True)
+mpl.rc('text.latex', preamble=r'\usepackage{cmbright}')
+mpl.rcParams.update({'font.size': 10})
+
+root = Tk.Tk()
+root.title("Diffraction at a finite periodic structure")
+
+def initialize():
+    kx0_double.set(0)
+    a_double.set(2)
+    b_double.set(6)
+    N_int.set(4)
+    log_N_F_double.set(np.log(.1))
+    propagation_string.set("exact")
+    
+    calculate()
+    
+def calculate():
+    a = a_double.get()
+    b = b_double.get()*a
+    kx0 = kx0_double.get()*2*np.pi/b
+    N = N_int.get()
+    A = (N-1)*b+2*a
+    A_string.set(round(A,2))
+    N_F = np.exp(log_N_F_double.get())
+    z_B = A**2/N_F
+    z_B_string.set(round(z_B,2))
+    propagation = propagation_string.get()
+    
+    Nx = 2**22
+    Lx = np.max([a*Nx/100.0/N_F,1000])
+    x, delta_x = np.linspace(-Lx/2,Lx/2,Nx,endpoint=False, retstep=True)
+    v = np.where(np.abs(x+a-A/2)<a,np.exp(1j*kx0*x),0)
+    for index in range(1,N):
+        v = v + np.where(np.abs(x+a-A/2+index*b)<a,np.exp(1j*kx0*x),0)    
+    a1.cla()        
+    a1.plot(x,np.abs(v)**2,'k')
+    a1.set_xlim([-np.max([A,10*a]),np.max([A,10*a])])
+    a1.set_ylim([-0.05,1.15])
+    a1.annotate(r'', xy=(-A/2,1.075), xytext=(-A/2-0.5*np.max([A,10*a]),1.075), arrowprops=dict(arrowstyle='->'))
+    a1.annotate(r'', xy=(A/2,1.075), xytext=(A/2+0.5*np.max([A,10*a]),1.075), arrowprops=dict(arrowstyle='->'))
+    a1.annotate(r'A', xy=(0,1.06),horizontalalignment='center', verticalalignment='center')
+    a1.set_xlabel(r'$x/\lambda$')
+    a1.set_ylabel(r'$|u(z=0)|^2$ [norm. u.]')
+
+    V = np.fft.fft(v)
+    kx = 2*np.pi*np.fft.fftfreq(Nx,delta_x)
+    k = 2*np.pi
+    a2.cla()    
+    a2.plot(np.fft.fftshift(kx)/k,(np.abs(np.fft.fftshift(V))/np.max(np.abs(V)))**2,'b')
+    a2.set_xlim([-1/a,1/a])
+    a2.set_xlabel(r'$k_x/k$')
+    a2.set_ylabel(r'$|U(z=0)|^2$ [norm. u.]')
+
+    if propagation == 'paraxial':
+        V = V*np.exp(-1j*kx**2/(2*k)*z_B)
+    else:
+        V = V*np.exp(1j*np.sqrt(k**2-kx**2+0j)*z_B)
+    v = np.fft.ifft(V)
+    a3.cla()
+    a3.plot(x/z_B,np.abs(v)**2,'b')
+    a3.set_xlim([-np.max([1/a,a1.get_xlim()[1]/z_B]),np.max([1/a,a1.get_xlim()[1]/z_B])])
+    a3.set_xlabel(r'$x/z_{\rm B}$')
+    a3.set_ylabel(r'$|u(z=z_{\rm B})|^2$ [norm. u.]')
+    
+    plt.tight_layout()
+    
+#    plt.savefig('FPS.pdf',bbox_inches='tight',dpi=300, transparent=True)
+    
+    canvas.draw()       
+
+f = plt.figure(1,[6,6.75])
+a1 = f.add_subplot(311)
+a2 = f.add_subplot(312)
+a3 = f.add_subplot(313)
+
+canvas = gui.create_canvas(root,f)
+mainframe = gui.create_mainframe(root)
+
+kx0_double = Tk.DoubleVar()
+a_double = Tk.DoubleVar()
+b_double = Tk.DoubleVar()
+N_int = Tk.IntVar()
+A_string = Tk.StringVar()
+log_N_F_double = Tk.DoubleVar()
+z_B_string = Tk.StringVar()
+propagation_string = Tk.StringVar()
+
+initialize()
+
+row = 1
+row = gui.create_slider_with_latex(mainframe,r"half slit width $a/\lambda =$",a_double,.25,4,row)
+row = gui.create_slider_with_latex(mainframe,r"period length $b/a =$",b_double,4,10,row)
+row = gui.create_intslider_with_latex(mainframe,r"Number of periods $N =$",N_int,1,7,row)
+row = gui.create_label_with_latex(mainframe,r"Aperture $A/\lambda =$",A_string,row)
+row = gui.create_logslider_with_latex(mainframe,r"Fresnel Number $N_{\rm F} = A^2/(\lambda z_{\rm B}) =$",log_N_F_double,0.1,1000,row)
+row = gui.create_label_with_latex(mainframe,r"Distance to screen $z_{\rm B} / \lambda =$",z_B_string,row)
+row = gui.create_spacer(mainframe,row)
+row = gui.create_slider_with_latex(mainframe,r"angle of incidence $b\sin(\varphi)/\lambda =$",kx0_double,-1,1,row)
+row = gui.create_spacer(mainframe,row)
+row = gui.create_radiobutton(mainframe,['vacuum propagation:','paraxial','exact'],propagation_string,2,row)
+row = gui.create_spacer(mainframe,row)
+row = gui.create_button(mainframe,"Calculate",calculate,row)
+
+gui.mainloop_safe_for_mac(root)
---
+++ gui_stuff.py
@@ -0,0 +1,297 @@
+import subprocess
+import platform
+import tkinter as Tk
+from tkinter import messagebox as mbox
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import ttk
+import sympy as sp
+from io import BytesIO
+from PIL import Image, ImageTk
+    
+def create_canvas(root,f):
+    canvas = FigureCanvasTkAgg(f, master=root)
+    canvas.get_tk_widget().grid(column=1, row=1, sticky=(Tk.W, Tk.E))
+    return canvas
+    
+def create_mainframe(root):
+    mainframe = ttk.Frame(root, padding="3 3 12 12")
+    mainframe.grid(column=2, row=1, sticky=(Tk.W, Tk.E))
+    return mainframe
+
+def latex2png(latex):
+    preamble = "\\documentclass[10pt]{article}\n" \
+               "\\usepackage{cmbright}\\usepackage{xcolor}\\pagestyle{empty}\\begin{document}\\pagecolor{lightgray}"
+    obj = BytesIO()
+    sp.preview(latex, viewer='BytesIO', output='png', outputbuffer=obj, preamble=preamble)
+    obj.seek(0)
+    img = Image.open(obj)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+    newData = []
+    for item in datas:
+        if item[0] == 191 and item[1] == 191 and item[2] == 191:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+    img.putdata(newData)
+    labelimage = ImageTk.PhotoImage(img)
+    return labelimage
+    
+def create_title(mainframe,text,row):
+    ttk.Label(mainframe, text=text).grid(column=2, row=row, sticky=Tk.W, padx=5, pady=5)
+    row=row+1
+    return row
+    
+def create_entry(mainframe,text,textvariable,row):
+    ttk.Label(mainframe, text=text).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(mainframe, width=7, textvariable=textvariable).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row
+
+def create_entry_with_latex(mainframe,latex,textvariable,row):
+    labelimage = latex2png(latex)
+    label = ttk.Label(mainframe, image=labelimage)
+    label.labelimage = labelimage
+    label.grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(mainframe, width=7, textvariable=textvariable).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row 
+
+def create_double_entry(mainframe,text1,textvariable1,text2,textvariable2,row):
+    Aframe1 = ttk.Frame(mainframe)
+    Aframe1.grid(column=1, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Label(Aframe1, text=text1).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(Aframe1, width=7, textvariable=textvariable1).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    Aframe2 = ttk.Frame(mainframe)
+    Aframe2.grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Label(Aframe2, text=text2).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(Aframe2, width=7, textvariable=textvariable2).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row
+
+def create_triple_entry(mainframe,text1,textvariable1,text2,textvariable2,text3,textvariable3,row):
+    Aframe1 = ttk.Frame(mainframe)
+    Aframe1.grid(column=1, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Label(Aframe1, text=text1).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(Aframe1, width=7, textvariable=textvariable1).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    Aframe2 = ttk.Frame(mainframe)
+    Aframe2.grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Label(Aframe2, text=text2).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(Aframe2, width=7, textvariable=textvariable2).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Label(Aframe2, text=text3).grid(column=3, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(Aframe2, width=7, textvariable=textvariable3).grid(column=4, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row
+
+def create_entry_with_image(mainframe,image,textvariable,row):
+    labelimage = Tk.PhotoImage(file=image)
+    label = ttk.Label(mainframe, image=labelimage)
+    label.labelimage = labelimage
+    label.grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(mainframe, width=7, textvariable=textvariable).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row
+    
+def create_spacer(mainframe,row):    
+    ttk.Label(mainframe, text=" ").grid(column=1, row=row, padx=5, pady=5)
+    row=row+1
+    return row
+    
+def create_button(mainframe,text,command,row):     
+    ttk.Button(mainframe, text=text, command=command).grid(column=2, row=row, sticky=Tk.W, padx=5, pady=5)
+    row=row+1
+    return row
+
+def create_launch_button(mainframe,filename,column,row):  
+    if platform.system()=='Windows':
+        command_string = "python "+filename
+    else:
+        command_string = ["python", filename]
+    def command():
+        subprocess.Popen(command_string)
+    ttk.Button(mainframe, text=filename, command=command).grid(column=column, row=row, padx=5, pady=5)
+    row=row+1
+    return row
+
+def create_checkbutton(mainframe,text,offvalue,onvalue,variable,row):
+    ttk.Checkbutton(mainframe, text=text, offvalue=offvalue, onvalue=onvalue, variable=variable).grid(column=2, row=row, sticky=Tk.W, padx=5, pady=5)
+    row=row+1
+    return row
+
+def create_radiobutton(mainframe,text,textvariable,N,row,optional_command='none'):
+    ttk.Label(mainframe, text=text[0]).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    Vframe = ttk.Frame(mainframe)
+    Vframe.grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    for index_row in range(int(np.ceil(N/2))):
+        for index_col in range(np.minimum(2,N-index_row*2)):
+            if optional_command=='none':
+                ttk.Radiobutton(Vframe, text=text[index_row*2+index_col+1], variable=textvariable, value=text[index_row*2+index_col+1]).grid(column=2*index_col+2, row=1+index_row*2, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+            else:
+                ttk.Radiobutton(Vframe, text=text[index_row*2+index_col+1], variable=textvariable, value=text[index_row*2+index_col+1], command=optional_command).grid(column=2*index_col+2, row=1+index_row*2, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row  
+
+def create_radiobutton_with_entry(mainframe,entrytext,entrytextvariable,text,textvariable,N,row,optional_command='none'):
+    Aframe1 = ttk.Frame(mainframe)
+    Aframe1.grid(column=1, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Label(Aframe1, text=entrytext).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Entry(Aframe1, width=7, textvariable=entrytextvariable).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    Vframe = ttk.Frame(mainframe)
+    Vframe.grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    for index_row in range(int(np.ceil(N/2))):
+        for index_col in range(np.minimum(2,N-index_row*2)):
+            if optional_command=='none':
+                ttk.Radiobutton(Vframe, text=text[index_row*2+index_col], variable=textvariable, value=text[index_row*2+index_col]).grid(column=2*index_col+2, row=1+index_row*2, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+            else:
+                ttk.Radiobutton(Vframe, text=text[index_row*2+index_col], variable=textvariable, value=text[index_row*2+index_col], command=optional_command).grid(column=2*index_col+2, row=1+index_row*2, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row  
+
+def create_slider(mainframe,text,variable,from_value,to_value,row,optional_command='none'):
+    ttk.Label(mainframe, text=text).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Label(mainframe, text=str(round(variable.get(),4))).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    def showvalue(value):
+        ttk.Label(mainframe, text=str(round(float(value),4))).grid(column=2, row=row-2, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+        if optional_command!='none':
+            optional_command(float(value))
+    ttk.Scale(mainframe, variable=variable, from_=from_value, to=to_value, command=showvalue, cursor='crosshair').grid(column=2, row=row+1, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+2
+    return row  
+
+def create_slider_with_latex(mainframe,latex,variable,from_value,to_value,row,optional_command='none'):
+    labelimage = latex2png(latex)
+    label = ttk.Label(mainframe, image=labelimage)
+    label.labelimage = labelimage
+    label.grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Label(mainframe, text=str(round(variable.get(),4))).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    def showvalue(value):
+        ttk.Label(mainframe, text=str(round(float(value),4))).grid(column=2, row=row-2, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+        if optional_command!='none':
+            optional_command(float(value))
+    ttk.Scale(mainframe, variable=variable, from_=from_value, to=to_value, command=showvalue, cursor='crosshair').grid(column=2, row=row+1, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+2
+    return row  
+
+def create_intslider_with_latex(mainframe,latex,variable,from_value,to_value,row,optional_command='none'):
+    labelimage = latex2png(latex)
+    label = ttk.Label(mainframe, image=labelimage)
+    label.labelimage = labelimage
+    label.grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Label(mainframe, text='%d' % variable.get()).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    def showvalue(value):
+        ttk.Label(mainframe, text='%d' % float(value)).grid(column=2, row=row-2, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+        if optional_command!='none':
+            optional_command(float(value))
+    ttk.Scale(mainframe, variable=variable, from_=from_value, to=to_value, command=showvalue, cursor='crosshair').grid(column=2, row=row+1, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+2
+    return row  
+
+def create_logslider_with_latex(mainframe,latex,variable,from_value,to_value,row,optional_command='none'):
+    labelimage = latex2png(latex)
+    label = ttk.Label(mainframe, image=labelimage)
+    label.labelimage = labelimage
+    label.grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Label(mainframe, text=str(round(np.exp(variable.get()),4))).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    def showvalue(value):
+        ttk.Label(mainframe, text=str(round(np.exp(float(value)),4))).grid(column=2, row=row-2, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+        if optional_command!='none':
+            optional_command(np.exp(float(value)))
+    ttk.Scale(mainframe, variable=variable, from_=np.log(from_value), to=np.log(to_value), command=showvalue, cursor='crosshair').grid(column=2, row=row+1, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+2
+    return row  
+    
+def create_entry_matrix_3x3_sym(mainframe,text,textvariable,row): 
+    ttk.Label(mainframe, text=text).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    Aframe = ttk.Frame(mainframe)
+    Aframe.grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Entry(Aframe, width=3, textvariable=textvariable[0]).grid(column=1, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Entry(Aframe, width=3, textvariable=textvariable[1]).grid(column=2, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Entry(Aframe, width=3, textvariable=textvariable[2]).grid(column=3, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Label(Aframe, textvariable=textvariable[3]).grid(column=1, row=2, sticky=(Tk.W, Tk.E))
+    ttk.Entry(Aframe, width=3, textvariable=textvariable[4]).grid(column=2, row=2, sticky=(Tk.W, Tk.E))
+    ttk.Entry(Aframe, width=3, textvariable=textvariable[5]).grid(column=3, row=2, sticky=(Tk.W, Tk.E))
+    ttk.Label(Aframe, textvariable=textvariable[6]).grid(column=1, row=3, sticky=(Tk.W, Tk.E))
+    ttk.Label(Aframe, textvariable=textvariable[7]).grid(column=2, row=3, sticky=(Tk.W, Tk.E))
+    ttk.Entry(Aframe, width=3, textvariable=textvariable[8]).grid(column=3, row=3, sticky=(Tk.W, Tk.E))
+    for child in Aframe.winfo_children(): child.grid_configure(padx=2, pady=2)   
+    row=row+1
+    return row
+
+def create_label(mainframe,text,textvariable,row):
+    ttk.Label(mainframe, text=text).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Label(mainframe, textvariable=textvariable).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row 
+
+def create_label_with_image(mainframe,image,textvariable,row):
+    labelimage = Tk.PhotoImage(file=image)
+    label = ttk.Label(mainframe, image=labelimage)
+    label.labelimage = labelimage
+    label.grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Label(mainframe, textvariable=textvariable).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row 
+
+def create_label_with_latex(mainframe,latex,textvariable,row):
+    labelimage = latex2png(latex)
+    label = ttk.Label(mainframe, image=labelimage)
+    label.labelimage = labelimage
+    label.grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    ttk.Label(mainframe, textvariable=textvariable).grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    row=row+1
+    return row 
+
+def create_label_vector_3(mainframe,text,textvariable,row):
+    ttk.Label(mainframe, text=text).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    Vframe = ttk.Frame(mainframe)
+    Vframe.grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Label(Vframe, text="(").grid(column=1, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Label(Vframe, textvariable=textvariable[0]).grid(column=2, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Label(Vframe, text=",").grid(column=3, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Label(Vframe, textvariable=textvariable[1]).grid(column=4, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Label(Vframe, text=",").grid(column=5, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Label(Vframe, textvariable=textvariable[2]).grid(column=6, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Label(Vframe, text=")").grid(column=7, row=1, sticky=(Tk.W, Tk.E))
+    row=row+1
+    return row   
+    
+def create_label_vector(mainframe,text,textvariable,N,row):
+    ttk.Label(mainframe, text=text).grid(column=1, row=row, sticky=Tk.E, padx=5, pady=5)
+    Vframe = ttk.Frame(mainframe)
+    Vframe.grid(column=2, row=row, sticky=(Tk.W, Tk.E), padx=5, pady=5)
+    ttk.Label(Vframe, text="(").grid(column=1, row=1, sticky=(Tk.W, Tk.E))
+    for index in range(N):
+        ttk.Label(Vframe, textvariable=textvariable[index]).grid(column=2*index+2, row=1, sticky=(Tk.W, Tk.E))
+        if index < N-1: ttk.Label(Vframe, text=",").grid(column=2*index+3, row=1, sticky=(Tk.W, Tk.E))
+    ttk.Label(Vframe, text=")").grid(column=2*N+1, row=1, sticky=(Tk.W, Tk.E))
+    row=row+1
+    return row  
+    
+def input_error(message,optional_command='none'):
+    mbox.showerror("Error", message)
+    if optional_command!='none':
+        optional_command()
+    pass
+
+def input_warning(message,optional_command='none'):
+    mbox.showwarning("Warning", message)
+    if optional_command!='none':
+        optional_command()
+    pass
+
+def create_stringvar_vector(N):
+    var_string = []
+    for index in range(N): var_string.append(Tk.StringVar())
+    return var_string
+
+def copy_stringvar_vector(var1_string,var2_string):
+    for index in range(len(var1_string)): var2_string[index].set(var1_string[index].get())
+
+def mainloop_safe_for_mac(root):
+    while True:
+        try:
+            root.mainloop()
+            break
+        except UnicodeDecodeError:
+            pass
