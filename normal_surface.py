a8b7df994dd80f40a583991f75d65cd8f2924632
Stefan Skupin <stefan.skupin@web.de>
2023-11-10 11:59:26 +0100

    use vectors for all Tk.StringVar and Tk.DoubleVar in normal_surface.py

--- normal_surface.py
+++ normal_surface.py
@@ -14,10 +14,10 @@ def initialize():
     var_string[2].set("3")   # epsilon3
     var_string[3].set("no_show")   # show E
     var_string[4].set("no_show")   # show S
-    theta0_double.set(1/4)
-    phi0_double.set(1/4)
-    theta_view_double.set(1/3)
-    phi_view_double.set(0.41)
+    var_double[0].set(1/4)  # theta0/pi
+    var_double[1].set(1/4)  # phi0/pi
+    var_double[2].set(1/3)  # theta_view/180
+    var_double[3].set(0.41) # phi_view/180
     calculate()
     
 def reinitialize():
@@ -28,10 +28,10 @@ def calculate():
     gui.change_cursor(root,"trek")
     try:
         epsilon = np.array([float(var_string[0].get()),float(var_string[1].get()),float(var_string[2].get())])
-        theta0 = theta0_double.get()*np.pi
-        phi0 = phi0_double.get()*np.pi
-        theta_view = theta_view_double.get()*180
-        phi_view = phi_view_double.get()*180
+        theta0 = var_double[0].get()*np.pi
+        phi0 = var_double[1].get()*np.pi
+        theta_view = var_double[2].get()*180
+        phi_view = var_double[3].get()*180
  
         if epsilon[0] <= 0 or epsilon[1] <= 0  or epsilon[2] <= 0: 
             gui.input_error("Tensor elements have to be positive. Re-initializing ...",reinitialize)
@@ -46,8 +46,8 @@ def calculate():
             ani.plot_ns(ax,theta0,phi0,epsilon,var_string[3].get(),var_string[4].get())
             
             na,nb = ani.dr(ani.uk(theta0,phi0),epsilon)
-            na_string.set(round(na[0],4))
-            nb_string.set(round(nb[0],4))
+            var_string[5].set(round(na[0],4))
+            var_string[6].set(round(nb[0],4))
                 
 #            plt.savefig('normal_surface.pdf',bbox_inches='tight',dpi=300, transparent=True)
             
@@ -63,14 +63,9 @@ canvas = gui.create_canvas(root,f)
 canvas.draw() # for faster feedback to user on startup
 mainframe = gui.create_mainframe(root)
 
-var_string = gui.create_stringvar_vector(5)
-var_save = gui.create_stringvar_vector(5)
-theta0_double = Tk.DoubleVar()
-phi0_double = Tk.DoubleVar()
-theta_view_double = Tk.DoubleVar()
-phi_view_double = Tk.DoubleVar()
-na_string = Tk.StringVar()
-nb_string = Tk.StringVar()
+var_string = gui.create_stringvar_vector(7)
+var_save = gui.create_stringvar_vector(7)
+var_double = gui.create_doublevar_vector(4)
 
 initialize()
 
@@ -79,14 +74,14 @@ row = gui.create_entry_with_latex(mainframe,r"Dielectric tensor element $\vareps
 row = gui.create_entry_with_latex(mainframe,r"Dielectric tensor element $\varepsilon_2=$",var_string[1],row)
 row = gui.create_entry_with_latex(mainframe,r"Dielectric tensor element $\varepsilon_3=$",var_string[2],row)
 row = gui.create_spacer(mainframe,row)
-row = gui.create_slider_with_latex(mainframe,r'Azimuth of propagation direction $\varphi_0/\pi=$',phi0_double,0,2,row)
-row = gui.create_slider_with_latex(mainframe,r'Elevation of propagation direction $\theta_0/\pi=$',theta0_double,0,1,row)
+row = gui.create_slider_with_latex(mainframe,r'Azimuth of propagation direction $\varphi_0/\pi=$',var_double[1],0,2,row)
+row = gui.create_slider_with_latex(mainframe,r'Elevation of propagation direction $\theta_0/\pi=$',var_double[0],0,1,row)
 row = gui.create_spacer(mainframe,row)
-row = gui.create_label_with_latex(mainframe,r'index $n_a=$',na_string,row)
-row = gui.create_label_with_latex(mainframe,r'index $n_b=$',nb_string,row)
+row = gui.create_label_with_latex(mainframe,r'index $n_a=$',var_string[5],row)
+row = gui.create_label_with_latex(mainframe,r'index $n_b=$',var_string[6],row)
 row = gui.create_spacer(mainframe,row)
-row = gui.create_slider_with_latex(mainframe,r'Azimuth of view $\varphi_{\rm view}/\pi=$',phi_view_double,0,2,row)
-row = gui.create_slider_with_latex(mainframe,r'Elevation of view $\theta_{\rm view}/\pi=$',theta_view_double,0,1,row)
+row = gui.create_slider_with_latex(mainframe,r'Azimuth of view $\varphi_{\rm view}/\pi=$',var_double[3],0,2,row)
+row = gui.create_slider_with_latex(mainframe,r'Elevation of view $\theta_{\rm view}/\pi=$',var_double[2],0,1,row)
 row = gui.create_spacer(mainframe,row)
 row = gui.create_double_checkbutton_with_latex(mainframe,r'show $\mathbf{E}^{a,b}$','no_show','show',var_string[3],r'show $\mathbf{S}^{a,b}$','no_show','show',var_string[4],row)
 row = gui.create_spacer(mainframe,row)
