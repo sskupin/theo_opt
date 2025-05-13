6e3bf37c65e9d634eb7fd167437bd3e240548b60
Stefan Skupin <stefan.skupin@web.de>
2025-05-13 17:26:28 +0200

    adopt to strat_stuff.py etc.

--- 3D_Bloch.py
+++ 3D_Bloch.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
 import gui_stuff as gui
+import strat_stuff as strat
 import media as media
 from matplotlib.colors import LogNorm
 
@@ -10,92 +11,65 @@ title = "3D DR of Bloch modes"
 root = Tk.Tk()
 root.title(title)
 
-def mTE(kfz,z):
-    if np.abs(kfz) == 0:
-        m = np.array([[1,z],[0,1]])
-    else:
-        m = np.array([[np.cos(kfz*z),np.sin(kfz*z)/kfz],[-np.sin(kfz*z)*kfz,np.cos(kfz*z)]])
-    return m
-
-def mTM(kfz,epsilon_f,z):
-    if np.abs(kfz) == 0:
-        m = np.array([[1,z*epsilon_f],[0,1]])
-    else:
-        m = np.array([[np.cos(kfz*z),np.sin(kfz*z)*epsilon_f/kfz],[-np.sin(kfz*z)*kfz/epsilon_f,np.cos(kfz*z)]])
-    return m    
-
 def DR(d1,epsilon_f1,d2,epsilon_f2,polarization,Kx,Omega): # computing dispersion relation for Bloch modes
     kf1z = np.sqrt(epsilon_f1+0j-(Kx/Omega)**2)*2*np.pi
     kf2z = np.sqrt(epsilon_f2+0j-(Kx/Omega)**2)*2*np.pi
     if polarization == 'TE':
-        m1 = mTE(kf1z,d1*Omega/(d1+d2))
-        m2 = mTE(kf2z,d2*Omega/(d1+d2))
+        m1 = strat.mTE(kf1z,d1*Omega/(d1+d2))
+        m2 = strat.mTE(kf2z,d2*Omega/(d1+d2))
     else:
-        m1 = mTM(kf1z,epsilon_f1,d1*Omega/(d1+d2))
-        m2 = mTM(kf2z,epsilon_f2,d2*Omega/(d1+d2))
+        m1 = strat.mTM(kf1z,epsilon_f1,d1*Omega/(d1+d2))
+        m2 = strat.mTM(kf2z,epsilon_f2,d2*Omega/(d1+d2))
     M = np.matmul(m2,m1)
     Kz = np.arccos((M[1,1]+M[0,0])/2)/(2*np.pi)
     
     return Kz
-    
-def epsilon(medium,lambdav):
-    if medium == "Vacuum":
-        epsilon_medium = (1+0j)*np.ones_like(lambdav)
-    elif medium == "AlAs":
-        epsilon_medium = media.AlAs(lambdav)
-    elif medium == "AlGaAs (70% Al)":
-        epsilon_medium = media.AlGaAs70(lambdav)
-    elif medium == "AlGaAs (31.5% Al)":
-        epsilon_medium = media.AlGaAs31(lambdav)
-    elif medium == "TiO2":
-        epsilon_medium = media.TiO2(lambdav)
-    elif medium == "fused silica":
-        epsilon_medium = media.silica(lambdav)
-    else:
-        print("Oops! Medium not known")
-        
-    return epsilon_medium
 
 def initialize():
-    lambda_min_string.set("400")
-    lambda_max_string.set("2400")
-    d1_string.set("75")
-    d2_string.set("90")
-    film1_string.set("fused silica")
-    film2_string.set("TiO2")
-    polarization_string.set("TE")
-    band_string.set("band")
-    cone_string.set("n")
-        
+    var_string[0].set("140") # thickness layer 1 in nm
+    var_string[1].set("fused silica") # layer 1 medium
+    var_string[2].set("180") # thickness layer 2 in nm
+    var_string[3].set("TiO2") # layer 2 medium
+    var_string[5].set("400") # minimum vacuum wavelength in nm
+    var_string[6].set("2400") # maximum vacuum wavelength in nm
+    var_string[7].set("TE")
+    var_string[8].set("band")
+    var_string[9].set("n")
+    gui.copy_stringvar_vector(var_string,var_save)
     calculate()
+    
+def reinitialize():
+    gui.copy_stringvar_vector(var_save,var_string)
+    calculate() 
 
 def calculate():
+    gui.change_cursor(root,"trek")
     try:
-        lambda_min = float(lambda_min_string.get())
-        lambda_max = float(lambda_max_string.get())
-        d1 = float(d1_string.get())
-        d2 = float(d2_string.get())
-        D_string.set(d1+d2)
-        film1 = film1_string.get()
-        film2 = film2_string.get()
-        polarization = polarization_string.get()
-        band = band_string.get()
-        cone = cone_string.get()        
+        lambda_min = float(var_string[5].get())
+        lambda_max = float(var_string[6].get())
+        d1 = float(var_string[0].get())
+        d2 = float(var_string[2].get())
+        var_string[4].set(d1+d2)
+        film1 = var_string[1].get()
+        film2 = var_string[3].get()
+        polarization = var_string[7].get()
+        band = var_string[8].get()
+        cone = var_string[9].get()        
         
         if d1 <= 0 or d2 < 0 or (d1+d2)/lambda_min- (d1+d2)/lambda_max > 1 or lambda_min < 400 or lambda_max > 2400 or lambda_min >= lambda_max:
-            gui.input_error(initialize)
+            gui.input_error("Values out of range. Re-initializing ...", reinitialize)
         else:
             if (film1 == "AlAs" or film1 == "AlGaAs (70% Al)" or film1 == "AlGaAs (31.5% Al)" or film2 == "AlAs" or film2 == "AlGaAs (70% Al)" or film2 == "AlGaAs (31.5% Al)" ) and lambda_min < 800:
-                print("Oops! Losses not negigible in this wavelength range ... adjusting ...")
+                gui.input_error("Oops! Losses not negigible in this wavelength range ... adjusting ...")
                 lambda_min = 800
                 lambda_max = 2400
-                lambda_min_string.set(lambda_min)
-                lambda_max_string.set(lambda_max)
+                var_string[5].set(lambda_min)
+                var_string[6].set(lambda_max)
             f.clf()
             Omega = np.linspace((d1+d2)/lambda_max, (d1+d2)/lambda_min, num=501, endpoint=True) # normalized frequency
             lambdav = (d1+d2)/Omega # vacuum wavelength in nm
-            epsilon_f1 = np.real(epsilon(film1,lambdav))
-            epsilon_f2 = np.real(epsilon(film2,lambdav))
+            epsilon_f1 = np.real(media.epsilon(film1,lambdav))
+            epsilon_f2 = np.real(media.epsilon(film2,lambdav))
             Kx = np.linspace(0, np.amax(np.maximum(np.sqrt(epsilon_f1),np.sqrt(epsilon_f2))*Omega), num=501, endpoint=True) # normalized transverse wavevector
             Kz = np.zeros([Omega.size,Kx.size]) + 0j
             vDR = np.vectorize(DR)
@@ -135,39 +109,34 @@ def calculate():
             
 #            plt.savefig('3D_Bloch.pdf',bbox_inches='tight',dpi=300, transparent=True)
 
+            gui.copy_stringvar_vector(var_string,var_save)
+
             canvas.draw()
-    except ValueError: gui.input_error(initialize)
+    except ValueError: gui.input_error("Unknown error. Re-initializing ...", reinitialize)
+    gui.change_cursor(root,"arrow")
 
 f = plt.figure(1,[6,4])
 canvas = gui.create_canvas(root,f)
 canvas.draw()
 mainframe = gui.create_mainframe(root)
 
-lambda_min_string = Tk.StringVar()
-lambda_max_string = Tk.StringVar()
-d1_string = Tk.StringVar()
-film1_string = Tk.StringVar()
-d2_string = Tk.StringVar()
-film2_string = Tk.StringVar()
-D_string = Tk.StringVar()
-polarization_string = Tk.StringVar()
-band_string = Tk.StringVar()
-cone_string = Tk.StringVar()
+var_string = gui.create_stringvar_vector(10)
+var_save = gui.create_stringvar_vector(10)
 
 initialize()
 
 row = 1
 
 row = gui.create_title(mainframe,"unit cell parameters",row)
-row = gui.create_entry(mainframe,u"film 1 thickness: d [nm] =",d1_string,row)
-row = gui.create_radiobutton(mainframe,['film 1 medium:','Vacuum','AlGaAs (70% Al)','AlAs','AlGaAs (31.5% Al)','TiO2','fused silica'],film1_string,6,row)
-row = gui.create_entry(mainframe,u"film 2 thickness: d [nm] =",d2_string,row)
-row = gui.create_radiobutton(mainframe,['film 2 medium:','Vacuum','AlGaAs (70% Al)','AlAs','AlGaAs (31.5% Al)','TiO2','fused silica'],film2_string,6,row)
-row = gui.create_label(mainframe,u"\u039b [nm] =",D_string,row)
-row = gui.create_double_entry(mainframe,u"\u03bb [nm] >",lambda_min_string,u"\u03bb [nm] <",lambda_max_string,row)
-row = gui.create_radiobutton(mainframe,['polarization:','TE','TM'],polarization_string,2,row)
-row = gui.create_radiobutton(mainframe,['show:','band','gap'],band_string,2,row)
-row = gui.create_checkbutton(mainframe,"mark area outside vacuum light cone",'n','y',cone_string,row)
+row = gui.create_entry(mainframe,u"film 1 thickness: d [nm] =",var_string[0],row)
+row = gui.create_radiobutton_with_latex(mainframe,[r'film 1 medium:',r'Vacuum',r'Al$_{0.7}$Ga$_{0.3}$GAs',r'AlAs',r'Al$_{0.315}$Ga$_{0.685}$As',r'TiO$_2$ ($\varepsilon_{\rm or}$)',r'fused silica'],['film 1 medium:','Vacuum','AlGaAs (70% Al)','AlAs','AlGaAs (31.5% Al)','TiO2','fused silica'],var_string[1],6,row)
+row = gui.create_entry(mainframe,u"film 2 thickness: d [nm] =",var_string[2],row)
+row = gui.create_radiobutton_with_latex(mainframe,[r'film 2 medium:',r'Vacuum',r'Al$_{0.7}$Ga$_{0.3}$GAs',r'AlAs',r'Al$_{0.315}$Ga$_{0.685}$As',r'TiO$_2$ ($\varepsilon_{\rm or}$)',r'fused silica'],['film 2 medium:','Vacuum','AlGaAs (70% Al)','AlAs','AlGaAs (31.5% Al)','TiO2','fused silica'],var_string[3],6,row)
+row = gui.create_label(mainframe,u"\u039b [nm] =",var_string[4],row)
+row = gui.create_double_entry(mainframe,u"\u03bb [nm] >",var_string[5],u"\u03bb [nm] <",var_string[6],row)
+row = gui.create_radiobutton(mainframe,['polarization:','TE','TM'],var_string[7],2,row)
+row = gui.create_radiobutton(mainframe,['show:','band','gap'],var_string[8],2,row)
+row = gui.create_checkbutton(mainframe,"mark area outside vacuum light cone",'n','y',var_string[9],row)
 row = gui.create_spacer(mainframe,row)
 row = gui.create_button(mainframe,"Calculate",calculate,row)
 
