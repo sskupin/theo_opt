54218ba520e085072d4c5fd41fcd02a96e3faf79
Stefan Skupin <stefan.skupin@web.de>
2025-09-23 15:33:43 +0200

    corrections with falk in srs.py

--- srs.py
+++ srs.py
@@ -6,12 +6,12 @@ import tkinter as Tk
 import gui_stuff as gui
 
 gui.set_rcParams()
+title = "Stimulated Raman Scattering - Stokes-anti-Stokes Coupling"
 root = Tk.Tk()
-root.title("SRS -- Stokes Anti-Stokes coupling in UDPA")
+root.title(title)
 
 def initialize():
-    global var_save
-    var_string[0].set(".1") # \Delta k_{nl} / g_R
+    var_string[0].set("1") # \Delta k_{nl} / g_R
     var_string[1].set("10") # L*g_R
     var_string[2].set("1.2") # \delta
     var_string[3].set("0.01") # \kappa_L/g_R
@@ -21,12 +21,14 @@ def initialize():
     calculate()
     
 def reinitialize():
-    global var_string
     gui.copy_stringvar_vector(var_save,var_string)
     calculate()
+    
+def show_manual():
+    gui.show_manual("taylor_series.png",title) 
+    
 
 def calculate():
-    global var_save
     gui.change_cursor(root,"trek")
     try:
         s = float(var_string[0].get())
@@ -107,13 +109,10 @@ initialize()
 row = 1
 row = gui.create_description(mainframe,'nonlinear phase mismatch:',row)
 row = gui.create_entry_with_latex(mainframe,r'$\Delta k_{\rm nl} / g_{\rm R} =$',var_string[0],row)
-row = gui.create_spacer(mainframe,row)
 row = gui.create_description(mainframe,'interaction length:',row)
 row = gui.create_entry_with_latex(mainframe,r'$g_{\rm R}L = $',var_string[1],row)
-row = gui.create_spacer(mainframe,row)
 row = gui.create_description(mainframe,'frequency ratio:',row)
 row = gui.create_entry_with_latex(mainframe,r'$\delta = $',var_string[2],row)
-row = gui.create_spacer(mainframe,row)
 row = gui.create_description(mainframe,'laser SM:',row)
 row = gui.create_entry_with_latex(mainframe,r'$\kappa_{\rm L}/ g_{\rm R}= $',var_string[3],row)
 row = gui.create_spacer(mainframe,row)
@@ -121,6 +120,6 @@ row = gui.create_description(mainframe,'initial condition:',row)
 row = gui.create_entry_with_latex(mainframe,r'$\omega_{\rm S}I_{\rm AS0} / (\omega_{\rm AS}I_{\rm S0} )= $',var_string[4],row)
 row = gui.create_entry_with_latex(mainframe,r'$\varphi_{\rm S0} +  \varphi_{\rm AS0} - 2 \varphi_{\rm L0} = $',var_string[5],row)
 row = gui.create_spacer(mainframe,row)
-row = gui.create_button(mainframe,"Calculate",calculate,row)
+row = gui.create_double_button(mainframe,"Manual",show_manual,"Calculate",calculate,row)
 
 gui.mainloop_safe_for_mac(root)
