8e318f838b6bcc9d6c091b1374e92830f66980b7
Stefan Skupin <stefan.skupin@web.de>
2025-12-04 10:19:14 +0100

    manual and aux

--- scalar_beam_prop.py
+++ scalar_beam_prop.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
+import sys
+sys.path.append('./aux')
 import gui_stuff as gui
 import bpm_stuff as bpm
 
@@ -24,7 +26,7 @@ def reinitialize():
     calculate() # because sliders may have changed
     
 def show_manual():
-    gui.show_manual("taylor_series.png",title)
+    gui.show_manual("man/scalar_beam_prop.png",title)
     
 def calculate():
     gui.change_cursor(root,"trek")
