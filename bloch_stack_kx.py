2994b35ecf0c2dc0c0d3858a05dee197a6213a2f
Stefan Skupin <stefan.skupin@web.de>
2025-12-04 14:21:31 +0100

    manual and aux

--- bloch_stack_kx.py
+++ bloch_stack_kx.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
+import sys
+sys.path.append('./aux')
 import strat_stuff as strat
 import gui_stuff as gui
 
@@ -55,7 +57,7 @@ def reinitialize():
     calculate()  
 
 def show_manual():
-    gui.show_manual("taylor_series.png",title)
+    gui.show_manual("man/bloch_stack_kx.png",title)
 
 def calculate():
     gui.change_cursor(root,"trek")
