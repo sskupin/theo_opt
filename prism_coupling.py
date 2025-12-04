7e99e46a54f5da6d6bf69a6b39a3417f3f500eb8
Stefan Skupin <stefan.skupin@web.de>
2025-12-04 16:01:10 +0100

    manual and aux

--- prism_coupling.py
+++ prism_coupling.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
+import sys
+sys.path.append('./aux')
 import gui_stuff as gui
 import bpm_stuff as bpm
 import film_stuff as film
@@ -17,7 +19,7 @@ def initialize():
     calculate()
 
 def show_manual():
-    gui.show_manual("taylor_series.png",title) 
+    gui.show_manual("man/prism_coupling.png",title) 
     
 def calculate():
     gui.change_cursor(root,"trek")
