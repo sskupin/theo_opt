37eda60007e2bbe26212c83bf932b9e7ff9bc29c
Stefan Skupin <stefan.skupin@web.de>
2025-12-04 11:02:33 +0100

    manual and aux

--- image_4f.py
+++ image_4f.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
+import sys
+sys.path.append('./aux')
 import gui_stuff as gui
 import ff_stuff as ff
 
@@ -19,7 +21,7 @@ def initialize():
     calculate()
     
 def show_manual():
-    gui.show_manual("taylor_series.png",title)
+    gui.show_manual("man/image_4f.png",title)
     
 def calculate():    
     gui.change_cursor(root,"trek")
