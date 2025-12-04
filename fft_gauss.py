459d1e9140e1ba5b5366e94d39b878b7319b76f4
Stefan Skupin <stefan.skupin@web.de>
2025-12-04 17:34:33 +0100

    manual and aux

--- fft_gauss.py
+++ fft_gauss.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
+import sys
+sys.path.append('./aux')
 import gui_stuff as gui
 
 gui.set_rcParams()
@@ -15,7 +17,7 @@ def initialize():
     calculate()
 
 def show_manual():
-    gui.show_manual("taylor_series.png",title) 
+    gui.show_manual("man/fft_gauss.png",title) 
 
 def calculate():
     gui.change_cursor(root,"trek")
