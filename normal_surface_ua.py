8ed8476081335463b71933e51aa4938d3b31ad66
Stefan Skupin <stefan.skupin@web.de>
2025-12-04 10:10:45 +0100

    manual and aux

--- normal_surface_ua.py
+++ normal_surface_ua.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
+import sys
+sys.path.append('./aux')
 import gui_stuff as gui
 import aniso_stuff as ani
 
@@ -22,7 +24,7 @@ def reinitialize():
     gui.copy_stringvar_vector(var_stringsave,var_string)
 
 def show_manual():
-    gui.show_manual("taylor_series.png",title)
+    gui.show_manual("man/normal_surface_ua.png",title)
     
 def calculate():
     gui.change_cursor(root,"trek")
