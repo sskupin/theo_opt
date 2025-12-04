fbffe5ec7fa1b4675737bf3f00720fb819825b7f
Stefan Skupin <stefan.skupin@web.de>
2025-12-04 14:38:10 +0100

    manual and aux

--- fp_lambda.py
+++ fp_lambda.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
+import sys
+sys.path.append('./aux')
 import gui_stuff as gui
 import strat_stuff as strat
 import media as media
@@ -46,7 +48,7 @@ def reinitialize():
     calculate()  
 
 def show_manual():
-    gui.show_manual("taylor_series.png",title)
+    gui.show_manual("man/fp_lambda.png",title)
     
 def calculate():
     gui.change_cursor(root,"trek")
