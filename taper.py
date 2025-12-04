d49613dbd9561266c6e4cf6d65b69f5dcfb62ee5
Stefan Skupin <stefan.skupin@web.de>
2025-12-04 16:07:01 +0100

    manual and aux

--- taper.py
+++ taper.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as Tk
+import sys
+sys.path.append('./aux')
 import gui_stuff as gui
 import bpm_stuff as bpm
 import film_stuff as film
@@ -16,7 +18,7 @@ def initialize():
     calculate()
     
 def show_manual():
-    gui.show_manual("taylor_series.png",title) 
+    gui.show_manual("man/taper.png",title) 
     
 def calculate():
     gui.change_cursor(root,"trek")
